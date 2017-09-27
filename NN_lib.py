6ab2d8f57b244486ac345d4551d7c05d576e6c6c
Jason Chang <pyo0220@gmail.com>
2017-09-27 13:41:50 -0700

    for tutorial

--- NN_lib.py
+++ NN_lib.py
@@ -1,36 +1,68 @@
-import numpy as np
-import lsqfit
-import gvar as gv
-import matplotlib.pyplot as plt
+# two nucleon library
 import h5py as h5
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_data(h5file):
+    data = dict()
+    for s in ['a','b']:
+        data[s] = dict()
+        data[s]['cfg'] = np.array(h5file['cl3_24_48_b6p1_m0p2450'][s]['cfgs'])
+        data[s]['nucleon'] = np.squeeze(np.array(h5file['cl3_24_48_b6p1_m0p2450'][s]['nucleon']['nsq_0']))
+        data[s]['nn_I0'] = dict()
+        for q in range(4):
+            data[s]['nn_I0']['nsq_%s' %q] = np.squeeze(np.array(h5file['cl3_24_48_b6p1_m0p2450'][s]['nn_I0']['T1_plus_3S1']['mu_avg']['nsq_%s' %q]))
+        data[s]['nn_I1'] = dict()
+        for q in range(4):
+            data[s]['nn_I1']['nsq_%s' %q] = np.squeeze(np.array(h5file['cl3_24_48_b6p1_m0p2450'][s]['nn_I1']['A1_plus_1S0']['mu_avg']['nsq_%s' %q]))
+    return data
+
+def plot_meff(gvdata):
+    fig = plt.figure('effective mass',figsize=(7,4.326237))
+    ax = plt.axes([0.15,0.15,0.8,0.8])
+    x = np.arange(len(gvdata)//2)
+    meff = np.log(gvdata/np.roll(gvdata,-1))[x]
+    ax.errorbar(x = x, y = [d.mean for d in meff], yerr = [d.sdev for d in meff], ls='None', marker='o', capsize=2, fillstyle='none')
+    plt.draw()
+
+def plot_scor(gvdata):
+    fig = plt.figure('scaled correlator',figsize=(7,4.326237))
+    ax = plt.axes([0.15,0.15,0.8,0.8])
+    x = np.arange(len(gvdata)//2)
+    meff = np.log(gvdata/np.roll(gvdata,-1))[x]
+    scor = gvdata[x]*np.exp(meff*x)
+    ax.errorbar(x = x, y = [d.mean for d in scor], yerr = [d.sdev for d in scor], ls='None', marker='o', capsize=2, fillstyle='none')
+    plt.draw()
 
 class Fit(object):
-    def __init__(self,nstates):
-        self.n = nstates
-    def E(self,p,i):
-        En = p['E0']
-        for j in range(1,i):
-            En += np.exp(p['E%s' %j])
-        return En
-    def Z(self,p,i,s):
-        return p['Z%s_%s' %(i,s)]
-    def twopt(self,x,p,src,snk):
+    def __init__(self,params):
+        self.nstates = params['nstates']
+        self.key = params['data_select']
+    def priors(self,prior):
+        p = dict()
+        for d in self.key:
+            for q in prior:
+                if (set(d.split('_')).issubset(q.split('_')) or q.split('_')[-1][0] is 'E') and int(q[-1]) < self.nstates:
+                    p[q] = prior[q]
+        return p
+    def En(self,p,n,ag):
+        E = p['%s_E0' %ag]
+        for i in range(1,n+1):
+            E += np.exp(p['%s_E%s' %(ag,i)])
+        return E
+    def An(self,p,n,tag):
+        A = p['%s_A%s' %(tag,n)]
+        return A
+    def two_point_correlator(self,x,p,tag):
+        ag = tag.replace('new_','').replace('old_','').replace('_local','').replace('_nonlocal','')
         r = 0
-        for i in range(self.n):
-            En = self.E(p,i)
-            Zn_i = self.Z(p,i,src)
-            Zn_f = self.Z(p,i,snk)
-            r += Zn_i*Zn_f*np.exp(-En*x)
+        for n in range(self.nstates):
+            E = self.En(p,n,ag)
+            A = self.An(p,n,tag) 
+            r += A*np.exp(-E*x)
         return r
-    def twopt_ssps(self,x,p):
-        r=dict()
-        r['SS'] = self.twopt(x,p,'s','s')
-        r['PS'] = self.twopt(x,p,'s','p')
+    def fit_function(self,x,p):
+        r = dict()
+        for k in self.key:
+            r[k] = self.two_point_correlator(x,p,k)
         return r
-
-if __name__=='__main__':
-    fpath = './data/nn_new_cl3_b6p1_m0p02450.h5'
-    dpath = '/cl3_24_48_b6p1_m0p2450/a/nn_I0/T1_plus_3S1/mu_0/nsq_0'
-    Aclass = Analysis(fpath,dpath)
-    Aclass.read_data()
-    print(Aclass.data)
